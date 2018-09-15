725c69ec10028ebe0073ade42a0b18931e4d84ba
Vladimir Offitserov <vladimir@ob-vious.com>
2018-09-15 15:54:46 +0300

    first commit :tada:

---
+++ nuxt.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+    /*
+    ** Headers of the page
+    */
+    head: {
+        title: 'test',
+        meta: [
+            {charset: 'utf-8'},
+            {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+            {hid: 'description', name: 'description', content: ''}
+        ],
+        link: [
+            {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
+        ]
+    },
+    /*
+    ** Customize the progress bar color
+    */
+    loading: {color: '#2db9f0'},
+    modules: [
+        '@nuxtjs/apollo'
+    ],
+    apollo: {
+        tokenName: 'authorization',
+        authenticationType: 'Bearer',
+        clientConfigs: {
+            default: {
+                httpEndpoint: 'http://localhost:3001'
+            }
+        }
+    }
+}
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>{{ hello }}</h1>
+    </div>
+</template>
+<script>
+export default {
+    data () {
+        return {
+            hello: 'World'
+        }
+    }
+}
+</script>
